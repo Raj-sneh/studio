0669c31c806d24a1c747c5f1ac963d743bc3888e
Sneh Kumar Verma <snehkumarverma2011@gmail.com>
2025-11-15 16:05:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4164192500-5d49e",
+  "appId": "1:343362949002:web:9573de37206fefae564273",
+  "apiKey": "AIzaSyCBQfoKhWruhQs2Zb1wIcRBKNb7Fki4A74",
+  "authDomain": "studio-4164192500-5d49e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "343362949002"
+};
