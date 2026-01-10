b5008d4ec07861fd3997dcdd6d26f4486953bb39
Sneh Kumar Verma <snehkumarverma2011@gmail.com>
2026-01-10 08:35:56 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: could not load u

--- src/lib/samplers.ts
+++ src/lib/samplers.ts
@@ -12,7 +12,10 @@ const samplerUrls: Record<string, Record<string, string>> = {
         C3: "C3.mp3", "D#3": "Ds3.mp3", "F#3": "Fs3.mp3", A3: "A3.mp3", C4: "C4.mp3"
     },
     guitar: {
-        A3: "A3.mp3", C4: "C4.mp3", E4: "E4.mp3", G4: "G4.mp3"
+        A2: "A2.mp3",
+        C3: "C3.mp3",
+        E3: "E3.mp3",
+        "G#3": "Gs3.mp3",
     },
     flute: {
         A4: "A4.mp3", C4: "C4.mp3", E5: "E5.mp3", A5: "A5.mp3"
@@ -34,7 +37,7 @@ const samplerUrls: Record<string, Record<string, string>> = {
 
 const baseUrlMap: Record<string, string> = {
     piano: 'https://tonejs.github.io/audio/salamander/',
-    guitar: '/vsda/',
+    guitar: 'https://tonejs.github.io/audio/guitar-acoustic/',
     flute: '/vsda/',
     saxophone: '/vsda/',
     violin: '/vsda/',
