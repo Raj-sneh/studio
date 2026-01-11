ea58c2a05a64b9c571f04e3d0bed6455598098c8
Sneh Kumar Verma <snehkumarverma2011@gmail.com>
2026-01-11 15:03:19 +0000

    Deployed from Firebase Studio 11 Jan 2026, 20:13:29 Misconfigured secret

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-4164192500-5d49e",
+  "projectId": "studio-4164192500-df01a",
   "appId": "1:343362949002:web:9573de37206fefae564273",
   "apiKey": "AIzaSyCBQfoKhWruhQs2Zb1wIcRBKNb7Fki4A74",
-  "authDomain": "studio-4164192500-5d49e.firebaseapp.com",
+  "authDomain": "studio-4164192500-df01a.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "343362949002"
 };
