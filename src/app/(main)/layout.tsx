0b3de906f1358c5ae08ae4ff2a1d03347cef3f70
Sneh Kumar Verma <snehkumarverma2011@gmail.com>
2026-01-11 11:36:20 +0000

    you putted bot in explore page i need in welcome paage

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -10,7 +10,6 @@ export default function MainLayout({
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-1 container mx-auto px-4 py-8">{children}</main>
-      {process.env.NODE_ENV === 'development' && <AIBot />}
     </div>
   );
 }
