fd42852740f2864c0ae2778dddb619b9b4f608bf
Sneh Kumar Verma <snehkumarverma2011@gmail.com>
2026-03-18 17:10:51 +0000

    final fix build

--- src/app/suite/AIComposer.tsx
+++
@@ -1,2 +0,0 @@
-// Redirecting to the central component in src/components/suite
-export { AIComposer } from '@/components/suite/AIComposer';
--- src/components/suite/AiComposer.tsx
+++
@@ -1,3 +0,0 @@
-'use client';
-// This file is simplified to avoid casing conflicts with AIComposer.tsx
-export { AIComposer as AiComposer } from './AIComposer';
